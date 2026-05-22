a896181db38dec5aa64e1e93e26ac972446c9102
Konststep <Rain1-1@mail.ru>
2026-05-22 21:07:48 +0300

    Add files via upload

---
+++ zadanie1.12.3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <clocale>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int size = 0;
+    ofstream fout("out.txt");
+    cout << "Введите размер массива: ";
+    cin >> size;
+    fout << size << endl;
+
+    int* mas = new int[size]();
+    for (int i = 0; i < size; i++) {
+        cout << "MAS[" << i << "] = ";
+        cin >> mas[i];
+    }
+
+    for (int i = size - 1; i > -1; i--) {
+        fout << mas[i] << " ";
+    }
+    fout.close();
+    delete mas;
+    return EXIT_SUCCESS;
+}
