a13f6f64ae16c9dc273aa9fe43660e30a15838b9
Konststep <Rain1-1@mail.ru>
2026-05-22 16:48:27 +0300

    Add files via upload

---
+++ zadanie1.12.1.cpp
@@ -0,0 +1,27 @@
+﻿﻿#include <iostream>
+#include <clocale>
+#include <string>
+#include <Windows.h>
+#include < fstream >
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    //setlocale(LC_CTYPE, "ru_RU.UTF-8");
+    //SetConsoleCP(CP_UTF8);
+    //SetConsoleOutputCP(CP_UTF8);
+    string str;
+    ifstream fin("C:\\in.txt");
+
+    while (!fin.eof())
+    {
+        fin >> str;
+        cout << str << std::endl;
+    }
+    fin.close();
+
+    return EXIT_SUCCESS;
+}
