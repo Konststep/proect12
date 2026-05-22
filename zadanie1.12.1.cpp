06a117ad967be926bc2e753f413b1a4df3177760
Konststep <Rain1-1@mail.ru>
2026-05-22 22:17:24 +0300

    Update zadanie1.12.1.cpp

--- zadanie1.12.1.cpp
+++ zadanie1.12.1.cpp
@@ -7,12 +7,12 @@
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "RUS");
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    //setlocale(LC_CTYPE, "ru_RU.UTF-8");
-    //SetConsoleCP(CP_UTF8);
-    //SetConsoleOutputCP(CP_UTF8);
+    //setlocale(LC_ALL, "RUS");
+    //SetConsoleCP(1251);
+    //SetConsoleOutputCP(1251);
+    setlocale(LC_CTYPE, "ru_RU.UTF-8");
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
     string str;
     ifstream fin("C:\\in.txt");
 
