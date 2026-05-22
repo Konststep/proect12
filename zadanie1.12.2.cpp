04f13e1015badf3040753929256c1441221fee41
Konststep <Rain1-1@mail.ru>
2026-05-22 20:29:31 +0300

    Add files via upload

---
+++ zadanie1.12.2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <clocale>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+   
+    string str;
+    ifstream fin("C:\\in.txt");
+
+    fin >> str;
+    int size = stoi(str);
+    int* mas = new int[size];
+    
+    for (int i = 0; i < size; i++) {
+        fin >> str;
+        mas[i] = stoi(str);
+
+    }
+    fin.close();
+    int a = size;
+
+    for (int i = 0; i < size / 2; i++) {
+        a--;
+        swap(mas[i], mas[a]);
+    }
+
+    for (int i = 0; i < size; i++) {
+
+        cout << mas[i] << " ";
+    }
+    delete mas;
+    
+    return EXIT_SUCCESS;
+}
